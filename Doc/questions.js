90d942ca792a64d419d8549b4dc80b1a96582241
klaxmanbabu <58721601+klaxmanbabu@users.noreply.github.com>
2026-03-09 17:15:21 +0530

    Create questions.js

---
+++ Doc/questions.js
@@ -0,0 +1,94 @@
+window.QUESTION_BANK = [
+ {
+      "id": "Q1",
+      "question": "What risk activity mandates prequalification requirement for ESP/Contractor?",
+      "options": {
+        "A": "High",
+        "B": "Low",
+        "C": "Medium"
+      },
+      "answer": {
+        "option": "A",
+        "text": "High"
+      }
+    },
+    {
+      "id": "Q2",
+      "question": "Which team manages the contractor safety pre qualification?",
+      "options": {
+        "A": "EPRM",
+        "B": "HSE EPRM",
+        "C": "Suppliers"
+      },
+      "answer": {
+        "option": "B",
+        "text": "HSE EPRM"
+      }
+    },
+    {
+      "id": "Q3",
+      "question": "What is one example of a high-risk activity involving elevation?",
+      "options": {
+        "A": "Gardening",
+        "B": "Work at Height",
+        "C": "Office housekeeping"
+      },
+      "answer": {
+        "option": "B",
+        "text": "Work at Height"
+      }
+    },
+    {
+      "id": "Q4",
+      "question": "What type of space is listed as a high-risk activity?",
+      "options": {
+        "A": "Excavation",
+        "B": "Worke at height",
+        "C": "Confined Space"
+      },
+      "answer": {
+        "option": "C",
+        "text": "Confined Space"
+      }
+    },
+    {
+      "id": "Q5",
+      "question": "By which process HSE EPRM team ensures high risk managing contractors are assessed",
+      "options": {
+        "A": "Explosives",
+        "B": "Prequalification",
+        "C": "Lifting"
+      },
+      "answer": {
+        "option": "B",
+        "text": "Prequalification"
+      }
+    },
+    {
+      "id": "Q6",
+      "question": "What platform triggers and manages the pre qualification workflow?",
+      "options": {
+        "A": "SNOW",
+        "B": "HSE Hub",
+        "C": "Novartis Hub"
+      },
+      "answer": {
+        "option": "A",
+        "text": "SNOW"
+      }
+    },
+    {
+      "id": "Q7",
+      "question": "ESP stands for",
+      "options": {
+        "A": "External Server Process",
+        "B": "External Sequence Process",
+        "C": "External Service Provider"
+      },
+      "answer": {
+        "option": "C",
+        "text": "External Service Provider"
+      }
+    }
+  ]
+}
